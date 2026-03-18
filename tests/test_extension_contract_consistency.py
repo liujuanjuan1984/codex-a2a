4d781f4dfa37671d5f5baed2b5e27df1cb717f89
helen@cloud <liujuanjuan1984@gmail.com>
2026-03-17 23:58:22 -0400

    style: apply ruff formatting to contract consistency test

--- tests/test_extension_contract_consistency.py
+++ tests/test_extension_contract_consistency.py
@@ -83,7 +83,9 @@ async def test_session_query_runtime_result_envelope_matches_declared_contract(
     settings = make_settings(a2a_bearer_token="t-1", a2a_log_payloads=False, codex_timeout=1.0)
     card = build_agent_card(settings)
     ext_by_uri = {ext.uri: ext for ext in card.capabilities.extensions or []}
-    envelope_by_method = ext_by_uri[SESSION_QUERY_EXTENSION_URI].params["result_envelope"]["by_method"]
+    envelope_by_method = ext_by_uri[SESSION_QUERY_EXTENSION_URI].params["result_envelope"][
+        "by_method"
+    ]
     expected_envelope = envelope_by_method[method]
 
     dummy = DummyCodexClient(settings)
